6f9540088f894dec77d2fb7a1afe2847169d5440
agangwish <agangwish95@gmail.com>
2017-02-23 21:01:55 -0600

    Added some framework

--- minet-netclass/src/core/tcp_module.cc
+++ minet-netclass/src/core/tcp_module.cc
@@ -14,6 +14,7 @@
 #include <iostream>
 
 #include "Minet.h"
+#include "tcpstate.h"
 
 
 using std::cout;
@@ -24,6 +25,21 @@ using std::string;
 int main(int argc, char *argv[])
 {
   MinetHandle mux, sock;
+	ConnectionList<TCPState> conn_list;
+	int timeout = 1;
+	const char* stateNames[] = {"CLOSED",
+	      			"LISTEN",
+	      			"SYN_RCVD",
+	      			"SYN_SENT",
+	      			"SYN_SENT1",
+	      			"ESTABLISHED",
+	      			"SEND_DATA",
+	      			"CLOSE_WAIT",
+	      			"FIN_WAIT1",
+	      			"CLOSING",
+	      			"LAST_ACK",
+	      			"FIN_WAIT2",
+	      			"TIME_WAIT"};
 
   MinetInit(MINET_TCP_MODULE);
 
@@ -44,7 +60,7 @@ int main(int argc, char *argv[])
 
   MinetEvent event;
 
-  while (MinetGetNextEvent(event)==0) {
+  while (MinetGetNextEvent(event, timeout)==0) {
     // if we received an unexpected type of event, print error
     if (event.eventtype!=MinetEvent::Dataflow 
 	|| event.direction!=MinetEvent::IN) {
@@ -59,11 +75,12 @@ int main(int argc, char *argv[])
 	cerr << "IN MUX HANDLER\n\n";
         Packet p;
 	Connection c;
+	Buffer data_buf;
+	SockRequestResponse request;
+	SockRequestResponse response;
 	unsigned char tcp_flags, ip_flags, iph_len, tcph_len;
-	unsigned char &iph_len_ptr = iph_len;
-	unsigned char &tcph_len_ptr = tcph_len;
 	short unsigned int total_len, win_size;
-	unsigned int seq_num, ack_num;
+	unsigned int seq_num, ack_num, state;
         MinetReceive(mux,p);
         unsigned tcphlen=TCPHeader::EstimateTCPHeaderLength(p);
         cerr << "estimated header len="<<tcphlen<<"\n";
@@ -97,14 +114,76 @@ int main(int argc, char *argv[])
 	cerr << "\tACK NUM: " << ack_num << endl;
 	tcph.GetWinSize(win_size);	// update window size
 	cerr << "\tWINDOW SIZE: " << win_size << endl;
-	tcph.GetHeaderLen(tcph_len_ptr); 
-	cerr << "\tTCP header length: " << tcph_len << endl;
 	ipl.GetTotalLength(total_len);	// total packet size
 	cerr << "\tTotal Packet Length: " << total_len << endl;
-	ipl.GetHeaderLength(iph_len_ptr);	// IP header length
+	tcph.GetHeaderLen(tcph_len); 
+	cerr << "\tTCP header length: " << tcph_len << endl;
+	ipl.GetHeaderLength(iph_len);	// IP header length
 	cerr << "\tIP header length: " << iph_len << endl;	
-	total_len = total_len - tcphlen - iph_len;
+	total_len = total_len - (4 * (tcph_len + iph_len));
 	cerr << "\tTOTAL DATA LENGTH: " << total_len << endl;
+	data_buf = p.GetPayload().ExtractFront(total_len);
+	cerr << "\tDATA: " << data_buf << endl;
+
+	ConnectionList<TCPState>::iterator conn_list_iter = conn_list.FindMatching(c);
+	if (conn_list_iter == conn_list.end()) {
+		cerr << "\nCONNECTION WAS NOT IN LIST" << endl;
+		cerr << "\tAdding connection to list..." << endl;
+
+		TCPState server(1, LISTEN, 5);		// create state server
+		ConnectionToStateMapping<TCPState> new_CTSM(request.connection, Time(), server, false);	// mapping
+		conn_list.push_back(new_CTSM);		// add new entry to mapping
+		response.type = STATUS;
+		response.connection = request.connection;
+		response.bytes = 0;
+		response.error = EOK;
+		cerr << "\tSending Response..." << endl;
+		MinetSend(sock, response);
+		cerr << "\tResponse Sent via MinetSend" << endl;
+	}
+	state = conn_list_iter->state.GetState();	// get the current state of the connection
+	cerr << "\nCURRENT CONNECTION STATE: " << stateNames[state] << endl;
+
+	switch(state) {
+		case LISTEN:
+			cerr << "LISTENING in MUX..." << endl;
+			if (IS_SYN(tcp_flags)) {	// SYN packet was received
+				cerr << "\tSYN packet received" << endl;
+			}
+			cerr << "FINISHED LISTENING" << endl;
+			break;
+	      	case SYN_RCVD:
+			cerr << "SYN Received" << endl;
+			break;
+	      	case SYN_SENT:
+			cerr << "SYN Sent" << endl;
+			break;
+	      	case SYN_SENT1:
+			cerr << "SYN SENT 1" << endl;
+			break;
+	      	case ESTABLISHED:
+			cerr << "ESTABLISHED" << endl;
+			break;
+	      	case SEND_DATA:
+			cerr << "SEND DATA" << endl;
+			break;
+	      	case CLOSE_WAIT:
+			cerr << "CLOSE WAIT" << endl;
+			break;
+	      	case FIN_WAIT1:
+			cerr << "FIN WAIT 1" << endl;
+			break;
+	      	case CLOSING:
+			cerr << "CLOSING" << endl;
+			break;
+	      	case LAST_ACK:
+			cerr << "LAST ACK" << endl;
+			break;
+	      	case FIN_WAIT2:
+			cerr << "FIN WAIT 2" << endl;
+			break;
+	}
+
       }
 	/******************/	
 	/***** SOCKET *****/
